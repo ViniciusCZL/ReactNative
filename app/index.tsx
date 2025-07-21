601e0ae285ab36375fdce571f57cf4271a3f5660
ViniciusCZL <vinicius_cruz_lopes@hotmail.com>
2025-07-21 17:09:10 -0400

    testando

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,59 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold ">Bem vindo</Text>
+    <View className="flex flex-1 flex-col justify-center items-center">
+      
+      <View>
+
+      <Text className="text-5xl text-dark-200 font-bold ">Camisetas</Text>
+      </View>
+
+        <View className="flex flex-row gap-5 w-full flex-wrap">
+          
+          <View className="flex w-30 h-50 flex-col ml-1">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+
+          <View className="flex w-30 h-50 flex-col">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+
+          <View className="flex w-30 h-50 flex-col">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+          <View className="flex w-30 h-50 flex-col">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+
+          <View className="flex w-30 h-50 flex-col">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+
+          <View className="flex w-30 h-50 flex-col">
+            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
+              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
+            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          </View>
+        </View>
+
+
+      <Text className="text-5xl text-dark-200 font-bold marginTop:0 ">Bem vindo</Text>
       
     </View>
   );
