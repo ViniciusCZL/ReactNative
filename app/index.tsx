b19a994b7032a17bf2f9a1496215a1ba53813ce8
ViniciusCZL <vinicius_cruz_lopes@hotmail.com>
2025-07-25 16:57:06 -0400

    task1

--- app/index.tsx
+++ app/index.tsx
@@ -2,62 +2,85 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex flex-1 flex-col justify-center items-center">
+    <View className="flex-1 flex flex-col justify-center items-center overflow-auto bg-slate-100">
       
       <View>
-
-      <Text className="text-5xl text-dark-200 font-bold ">Camisetas</Text>
+        <Text className="text-5xl text-dark-200 font-bold m-14">Camisetas</Text>
       </View>
 
-        <View className="flex flex-row gap-5 w-full flex-wrap">
+        <View className="flex flex-row gap-5 w-full flex-wrap justify-center">
           
-          <View className="flex w-30 h-50 flex-col ml-1">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
           </View>
 
-          <View className="flex w-30 h-50 flex-col">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
           </View>
 
-          <View className="flex w-30 h-50 flex-col">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
-          </View>
-          <View className="flex w-30 h-50 flex-col">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
           </View>
 
-          <View className="flex w-30 h-50 flex-col">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
           </View>
 
-          <View className="flex w-30 h-50 flex-col">
-            <img className="flex w-40 h-40" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
-            <div className="text-2x1 text-dark-200 font-semibold justify-center items-center">Camiseta Polo</div>
-              <Text className="text-1x1 text-dark-100 font-light">Camiseta Polo preta jacacoste</Text>
-            <button className="flex-1 justify-center items-center hover:bg-green-400 bg-gray-500" >Comprar</button>
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
           </View>
-        </View>
 
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
+          </View>
 
-      <Text className="text-5xl text-dark-200 font-bold marginTop:0 ">Bem vindo</Text>
-      
+          <View className="flex w-30 h-50 flex-col ml-1 bg-slate-300 items-center rounded-lg">
+            <img className="flex w-32 h-38 mt-2" src="https://images.tcdn.com.br/img/img_prod/1082616/camisa_polo_poliester_preta_90002356_1_d754f5f043ed15a8d5176cd7f0d39f50.jpg" alt="" />
+            <div className="text-2x1 text-dark-200 font-semibold justify-wcenter items-center">Camiseta Polo</div>
+              <Text className="rounded-lg p-1 flex flex-wrap text-xs h-11 w-36 text-dark-500 font-light m-3 bg-slate-200">Camiseta Polo preta jacacoste</Text>
+            <div className="flex-1 justify-center items-center space-x-2">
+              <button className="text-sm  hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-2 pr-2">Comprar</button>
+              <button className="text-sm hover:bg-green-400 bg-gray-400 rounded-lg p-1 pl-1.5 pr-1.5 mb-2">Carrinho</button>
+            </div>
+          </View>
+
+      <Text className="text-5xl text-dark-200 font-bold marginTop:0 ">Bem vindo</Text></View>
     </View>
   );
 }
-
-
